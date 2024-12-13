8e77cddb0860c7b0aebfb2bc83f54ea3fb199da1
Luigi-Annic <luigi.annic@gmail.com>
2024-12-13 16:26:33 +0100

    caesar update

--- R/caesar.R
+++ R/caesar.R
@@ -20,8 +20,11 @@ caesar <- function(word, key, mode = "encrypt") {
   out <- character(length(w0))
   for (i in (1: length(w0))) {
     pos <- which(w0[i] == letters)
-    out[i] = letters[pos+ ifelse(mode == "encrypt" , key,
-                                 ifelse(mode == "decrypt", -key, "No mode"))]
+    newpos<- pos+ ifelse(mode == "encrypt" , key,
+                         ifelse(mode == "decrypt", -key, "No mode"))
+    finalpos0 <- ifelse(newpos>length(letters), newpos - length(letters), newpos) # correcting values >26
+    finalpos <- ifelse(finalpos0 < 1, finalpos0+length(letters), finalpos0)       # correcting values <0
+    out[i] = letters[finalpos]
   }
   
   final <- paste(out, collapse = "")
