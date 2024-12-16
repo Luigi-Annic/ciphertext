af9c2d45cec7383e4acd84e8ef8a882a1c74c663
Luigi-Annic <luigi.annic@gmail.com>
2024-12-16 16:20:53 +0100

    vigenere added, and decrypt option substitues mode

--- R/caesar.R
+++ R/caesar.R
@@ -4,7 +4,7 @@
 #'
 #' @param word Word or phrase to be encrypted
 #' @param key numeric key
-#' @param mode One of "decrypt" or "encrypt". Sets whether the program encrypts or decrypts the input word
+#' @param decrypt  If FALSE (default), the program ciphers the input word, If TRUE, the program decrypts it.
 #' 
 #' @return a string
 #' @export
@@ -13,15 +13,14 @@
 #' caesar("Hello", 1, "encrypt")
 #'
 
-caesar <- function(word, key, mode = "encrypt") {
+caesar <- function(word, key, decrypt = FALSE) {
   
   w0 <- PrepCyp.w(word)
   
   out <- character(length(w0))
   for (i in (1: length(w0))) {
     pos <- which(w0[i] == letters)
-    newpos<- pos+ ifelse(mode == "encrypt" , key,
-                         ifelse(mode == "decrypt", -key, "No mode"))
+    newpos<- pos+ ifelse(decrypt == FALSE , key, -key)
     finalpos0 <- ifelse(newpos>length(letters), newpos - length(letters), newpos) # correcting values >26
     finalpos <- ifelse(finalpos0 < 1, finalpos0+length(letters), finalpos0)       # correcting values <0
     out[i] = letters[finalpos]
