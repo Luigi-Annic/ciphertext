25b368b457f594760d5feb20fffe295492672025
Luigi-Annic <luigi.annic@gmail.com>
2024-12-18 11:25:59 +0100

    addspaces function added

---
+++ R/addspaces.R
@@ -0,0 +1,26 @@
+#' @title addspaces
+#'
+#' @description Internal function to add spaces between n letters in a string.
+#' This might be helpful when printing long sequences of ciphered text, 
+#'
+#' @param string phrase 
+#' @param L number of letters before a new space
+#' 
+#' @return a string
+#' @export
+#'
+#' @examples
+#' addspaces("abcdefghijklm", 5)
+#'
+
+
+addspaces<- function(string, L) {
+slice <- c()
+j<- 0
+for (i in seq(1, nchar(string), L)) {
+  j <- j+1
+  slice[j] <- substr(string, start= i, stop = i+L-1)
+}
+
+paste(slice, collapse = " ")
+}
