97371974baf5ab990c35c526309f4d47c28d3ff1
Luigi-Annic <luigi.annic@gmail.com>
2025-01-08 14:40:58 +0100

    minor updates

--- R/bifid_delastelle.R
+++ R/bifid_delastelle.R
@@ -8,16 +8,19 @@
 #' @param key key Word for creating the modified Polybius square
 #' @param period period length for splitting the input phrase. If greater or equal to the length of the input then the split is not executed
 #' 
+#' @inheritParams affine
+#' 
 #' @return a string
 #' @export
 #' 
 #' @examples
-#' bifid_delastelle("dcode", key = "secret", period = 3)
+#' bifid_delastelle("dcode", key = "secret", period = 3, encrypt = TRUE)
+#' bifid_delastelle("apiai", key = "secret", period = 3, encrypt = FALSE)
 #' 
 #' @references https://en.wikipedia.org/wiki/Bifid_cipher
 #'
 
-bifid_delastelle <- function(input, key = "", period = 100) {
+bifid_delastelle <- function(input, key = "", period = 100, encrypt = TRUE) {
   
 w0a <- PrepCyp.w(input)
 w0 <- ifelse(w0a == "j", "i", w0a) #transform j in i in input word
@@ -39,29 +42,34 @@ shuffledletters <- unique(c(key0, letters0))
 matpol <- matrix(shuffledletters, nrow = 5, byrow = TRUE)
 
 allblocks <- c()
-for (w in c(1:nrow(splitw0))) {
+
+
+for (w in c(1:nrow(splitw0))) { #process one block at a time
   
-w0 <- splitw0[w,]
+w0 <- splitw0[w,]  # focus on the current block
     
 coordinit <- c()
 
 for (i in 1:length(w0)) {
   coordinit_temp <- which(w0[i] == matpol, arr.ind = T)
-  coordinit <- rbind(coordinit, coordinit_temp)
+  coordinit <- rbind(coordinit, coordinit_temp) # create dataframe with initial coordinates
 }
 
-coordfin <- as.data.frame(matrix(as.numeric(coordinit), ncol = 2, byrow = TRUE))
+coordorder<- if(encrypt == TRUE) {as.numeric(coordinit)} else {as.numeric(t(coordinit))} #encryption/decryption defined here
+
+coordfin <- as.data.frame(matrix(coordorder, ncol = 2, byrow = isTRUE(encrypt))) # final coordinates, byrow is TRUE if encrypt, FALSE if decrypt
+
 out<- c()
 
 for (i in 1:nrow(coordfin)) {
- out_temp<- matpol[coordfin$V1[i],coordfin$V2[i]]
+ out_temp<- matpol[coordfin$V1[i],coordfin$V2[i]] # extract letters from coordinates
  out <- c(out, out_temp)
 }
 
-allblocks[w] <- paste(out, collapse = "")
+allblocks[w] <- paste(out, collapse = "") #save each transformed block
 
 }
 
-paste(allblocks, collapse = "")
+paste(allblocks, collapse = "") #paste together all blocks
 
 }
